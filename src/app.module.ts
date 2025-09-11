28e148836b5829441b02d27da5e252240eea7edc
Minh Tri <minhtrivo2005gg@gmail.com>
2025-09-12 02:23:33 +0700

    dung env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
-// import { Product } from './product/entities/product.entity';
 import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
 import { PhethaiModule } from './modules/phethai/phethai.module';
@@ -15,21 +13,27 @@ import { HopdonggiaodichModule } from './modules/hopdonggiaodich/hopdonggiaodich
     ConfigModule.forRoot({
       isGlobal: true, // Module Config có thể dùng toàn dự án
     }),
+    
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
+      useFactory: (config: ConfigService) => {
+      console.log('DB_USER:', config.get('DB_USER'));
+      console.log('DB_PASS:', config.get('DB_PASS'));
+      return {
         type: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'root',
-        password: 'dauphuthanhkim00',
-        database: 'vietcycleconnectv1',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT'), 10),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
         entities: [__dirname + '/entities/*.entity{.ts,.js}'],
         synchronize: false,
-        migrationsRun: false,
-      }),
+      };
+    }
+
     }),
+
 
     // post login , auth và get profile từng role từng người dùng 
     AccountModule,
